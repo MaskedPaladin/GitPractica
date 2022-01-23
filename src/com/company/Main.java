8567870a7fb49ed1078b36f03539be0b1bbfd8a8
MaskedPaladin <carjien@gmail.com>
2022-01-23 11:09:38 +0100

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("hola bonic mon");
 	    System.out.println("hola terrible mon");
     }
 }
